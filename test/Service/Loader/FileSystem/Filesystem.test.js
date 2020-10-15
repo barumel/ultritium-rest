0d1376157d82610e3c0d6bca2d42b00220ce9309
Manuel Kipfer <manuel.kipfer@bluewin.ch>
2020-10-15 19:18:49 +0200

    Adjust test

--- test/Service/Loader/FileSystem/Filesystem.test.js
+++ test/Service/Loader/FileSystem/Filesystem.test.js
@@ -7,7 +7,8 @@ describe('Test filesystem service loader', () => {
   it('Must return the services from ./services folder', async function() {
     const p = path.join(__dirname, 'services');
     const loader = LoaderFileSystem({ path: p });
+    const services = await loader.load();
 
-    console.log('LOLOLO', await loader.load());
+    expect(Object.keys(services)).toEqual(['main', 'user']);
   });
 });
